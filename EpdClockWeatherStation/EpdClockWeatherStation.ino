243e720bb28ace4b4683b012f94e1622fe70c5ce
Haydn <haydnady@gmail.com>
2020-08-26 17:49:53 -0400

    Created print test function

--- EpdClockWeatherStation/EpdClockWeatherStation.ino
+++ EpdClockWeatherStation/EpdClockWeatherStation.ino
@@ -1,34 +1,41 @@
-// Uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX.
-#include <GFX.h>
-
 #define ENABLE_GxEPD2_GFX 0
 #include <GxEPD2_BW.h> // including both doesn't hurt
 #include <GxEPD2_3C.h> // including both doesn't hurt
-#include "bitmaps/Bitmaps3c104x212.h" // 2.13" b/w/r
+#include "bitmaps/Bitmaps3c104x212.h" // 2.13" b/w/y
 
-#include <Fonts/FreeMonoBold9pt7b.h>
-
-// Constructor for e-paper, and for AVR needed #defines.
 GxEPD2_3C<GxEPD2_213c, GxEPD2_213c::HEIGHT> display(GxEPD2_213c(/*CS=15*/ SS, /*DC=4*/ 4, /*RST=5*/ 5, /*BUSY=16*/ 16));
 
+// Fonts
+#include <Fonts/FreeMonoBold9pt7b.h>
 
-//=====================================================================SETUP
+//===================================================================== SETUP
 void setup()
 {
   display.init();
-  // Comment out next line to have no or minimal Adafruit_GFX code.
+  display.setRotation(3);
+
+  printTest();
+ 
+}
+
+//===================================================================== PrintText
+void printTest()
+{
   display.setTextColor(GxEPD_BLACK);
+  display.setFont(&FreeMonoBold9pt7b);
+  
   display.firstPage();
   do
   {
-    display.fillScreen(GxEPD_WHITE);
-    // Comment out next line to have no or minimal Adafruit_GFX code.
+    display.fillScreen(GxEPD_YELLOW);
+    display.setCursor(0,15);
     display.print("Hello World!");
   }
   while (display.nextPage());
 }
 
-//=====================================================================LOOP
-void loop() {};
-
-//=====================================================================WIP
+//===================================================================== LOOP
+void loop() 
+{
+  
+}
